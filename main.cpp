f94d871d2f27b57cfdc231d66490dd7b4f6488d3
pjy0121 <qkrwldyd19@gmail.com>
2017-10-05 20:24:23 +0900

    1P/2P 개념 도입 - 방향키 셋팅, 시작 위치 설정

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 #include <iostream>
 #include <cmath>
 
+#define P1 true
+#define P2 false
+
 using namespace std;
 using namespace glm;
 
@@ -10,14 +13,82 @@ DigitalCanvas2D my_canvas("This is my digital canvas!", 1024, 768); // Canvas : 
 
 class Human
 {
+protected:
+	int attack_;
+	int defense_;
+	bool player_;
+
 public:
 	vec3 center_;
 	vec3 direction_;
+	int hp_;
+	
+
+	// 생성자
+	Human() : hp_(100), attack_(10), defense_(10)
+	{}
+
+	// 게임 시작 시 초기 위치 지정
+	void initPosition()
+	{
+		if (player_)
+		{
+			center_.x = -1;
+			center_.y = rand() % -1 - 0.5;
+		}
+		else
+		{
+			center_.x = -1;
+			center_.y = rand() % 1 + 0.5;
+		}
+	}
 
 	void setValues(vec3 _center, vec3 _direction)
 	{
 		center_ = _center;
 		direction_ = _direction;
+	}
+
+	bool keySettings()
+	{
+		if (player_)
+		{
+			// move left & right side
+			if (my_canvas.isKeyPressed(GLFW_KEY_LEFT)) {
+				center_.x -= 0.0004;
+			}
+			if (my_canvas.isKeyPressed(GLFW_KEY_RIGHT)) {
+				center_.x += 0.0004;
+			}
+
+			// move up & down
+			if (my_canvas.isKeyPressed(GLFW_KEY_UP)) {
+				center_.y += 0.0004;
+			}
+			if (my_canvas.isKeyPressed(GLFW_KEY_DOWN)) {
+				center_.y -= 0.0004;
+			}
+			return P1;
+		}
+		else
+		{
+			// move left & right side
+			if (my_canvas.isKeyPressed(GLFW_KEY_A)) {
+				center_.x += 0.0004;
+			}
+			if (my_canvas.isKeyPressed(GLFW_KEY_D)) {
+				center_.x -= 0.0004;
+			}
+
+			// move up & down
+			if (my_canvas.isKeyPressed(GLFW_KEY_W)) {
+				center_.y += 0.0004;
+			}
+			if (my_canvas.isKeyPressed(GLFW_KEY_S)) {
+				center_.y -= 0.0004;
+			}
+			return P2;
+		}
 	}
 
 	void drawHead(const vec3& color)
@@ -90,10 +161,20 @@ public:
 
 	void draw()
 	{
-		my_canvas.beginTransformation();
-		my_canvas.translate(center_.x, center_.y);
-		my_canvas.drawFilledBox(RGBColors::green, 0.1, 0.1);
-		my_canvas.endTransformation();
+		if (player_)
+		{
+			my_canvas.beginTransformation();
+			my_canvas.translate(center_.x, center_.y);
+			my_canvas.drawFilledBox(RGBColors::green, 0.1, 0.1);
+			my_canvas.endTransformation();
+		}
+		else
+		{
+			my_canvas.beginTransformation();
+			my_canvas.translate(center_.x, center_.y);
+			my_canvas.drawFilledBox(RGBColors::green, 0.1, 0.1);
+			my_canvas.endTransformation();
+		}
 	}
 };
 
@@ -111,11 +192,29 @@ public:
 		my_canvas.drawFilledBox(RGBColors::red, 0.15, 0.03);
 		my_canvas.endTransformation();
 	}
+
+	// 좌우대칭 함수
+	void reverseDraw()
+	{
+		my_canvas.scale(-1.0, 1.0);
+		draw();
+		my_canvas.scale(-1.0, 1.0);
+	}
 };
 
 class MyWarrior : public Human
 {
 public:
+	// 생성자
+	MyWarrior(bool _player)
+	{
+		player_ = _player;
+		attack_ = 20;
+		defense_ = 20;
+
+		initPosition();	// 시작 위치 설정
+	}
+
 	void drawRightArm(const float & time, const vec3 & color)
 	{
 		my_canvas.beginTransformation();
@@ -165,12 +264,114 @@ public:
 
 	void draw(const float& time)
 	{
+		if (!player_)	// 1P가 아니라면 좌우반전
+		{
+			my_canvas.scale(-1.0, 1.0);
+		}
 		drawHead(RGBColors::black);
 		drawBody(RGBColors::black);
-		if (my_canvas.isKeyPressed(GLFW_KEY_R)) actionLeft_1(time, RGBColors::black); else drawLeftArm(time, RGBColors::black);
-		if (my_canvas.isKeyPressed(GLFW_KEY_R)) actionRight_1(time, RGBColors::black); else drawRightArm(time, RGBColors::black);
+		if (player_)
+		{
+			if (my_canvas.isKeyPressed(GLFW_KEY_K)) actionLeft_1(time, RGBColors::black); else drawLeftArm(time, RGBColors::black);
+			if (my_canvas.isKeyPressed(GLFW_KEY_K)) actionRight_1(time, RGBColors::black); else drawRightArm(time, RGBColors::black);
+		}
+		else
+		{
+			if (my_canvas.isKeyPressed(GLFW_KEY_R)) actionLeft_1(time, RGBColors::black); else drawLeftArm(time, RGBColors::black);
+			if (my_canvas.isKeyPressed(GLFW_KEY_R)) actionRight_1(time, RGBColors::black); else drawRightArm(time, RGBColors::black);
+		}
 		drawLeftLeg(time, RGBColors::black);
 		drawRightLeg(time, RGBColors::black);
+		
+		if(!player_)	// 1P가 아니라면 좌우반전
+		{
+			my_canvas.scale(-1.0, 1.0);
+		}
+	}
+};
+
+class MyGunner : public Human
+{
+public:
+	MyGunner(bool _player)
+	{
+		player_ = _player;
+
+		initPosition();	// 시작 위치 설정
+	}
+
+	void drawRightArm(const float & time, const vec3 & color)
+	{
+		my_canvas.beginTransformation();
+		my_canvas.translate(center_.x + 0.02, center_.y - 0.05);
+		my_canvas.rotate(20);
+		my_canvas.scale(0.4, 0.1);
+		my_canvas.drawFilledBox(color, 0.2, 0.18);
+		my_canvas.endTransformation();
+	}
+
+	void drawLeftArm(const float & time, const vec3 & color)
+	{
+		my_canvas.beginTransformation();
+		my_canvas.translate(center_.x + 0.02, center_.y - 0.065);
+		my_canvas.scale(0.4, 0.1);
+		my_canvas.drawFilledBox(color, 0.2, 0.18);
+		my_canvas.translate(0.05, -0.06);
+		drawSword(time, RGBColors::red);
+		my_canvas.endTransformation();
+	}
+
+	// 오른손 스윙
+	void actionRight_1(const float & time, const vec3 & color)
+	{
+		my_canvas.beginTransformation();
+		my_canvas.translate(center_.x, center_.y);
+		my_canvas.rotate(sin(time*5.0) * 50.0);			// animation!
+		my_canvas.translate(0.04, -0.03);
+		my_canvas.scale(0.4, 0.1);
+		my_canvas.drawFilledBox(color, 0.18, 0.18);
+		my_canvas.endTransformation();
+	}
+
+	// 왼손 스윙
+	void actionLeft_1(const float & time, const vec3 & color)
+	{
+		my_canvas.beginTransformation();
+		my_canvas.translate(center_.x, center_.y);
+		my_canvas.rotate(sin(time*5.0) * 60.0);			// animation!
+		my_canvas.translate(0.03, -0.039);
+		my_canvas.scale(0.4, 0.1);
+		my_canvas.drawFilledBox(color, 0.18, 0.18);
+		my_canvas.translate(0.02, 0.0);
+		drawSword(time, RGBColors::red);
+		my_canvas.endTransformation();
+	}
+
+	void draw(const float& time)
+	{
+		if (!player_)	// 1P가 아니라면 좌우반전
+		{
+			my_canvas.scale(-1.0, 1.0);
+		}
+		drawHead(RGBColors::black);
+		drawBody(RGBColors::black);
+		if (player_)
+		{
+			if (my_canvas.isKeyPressed(GLFW_KEY_K)) actionLeft_1(time, RGBColors::black); else drawLeftArm(time, RGBColors::black);
+			if (my_canvas.isKeyPressed(GLFW_KEY_K)) actionRight_1(time, RGBColors::black); else drawRightArm(time, RGBColors::black);
+		}
+		else
+		{
+			if (my_canvas.isKeyPressed(GLFW_KEY_R)) actionLeft_1(time, RGBColors::black); else drawLeftArm(time, RGBColors::black);
+			if (my_canvas.isKeyPressed(GLFW_KEY_R)) actionRight_1(time, RGBColors::black); else drawRightArm(time, RGBColors::black);
+		}
+		drawLeftLeg(time, RGBColors::black);
+		drawRightLeg(time, RGBColors::black);
+
+		if (!player_)	// 1P가 아니라면 좌우반전
+		{
+			my_canvas.scale(-1.0, 1.0);
+		}
 	}
 };
 
@@ -210,44 +411,15 @@ int main(void)
 {
 	float time = 0.0;
 
-	MyTank tank;
-	MyWarrior warrior;
+	MyWarrior warrior(P1);
+	MyGunner gunner(P2);
 	MyBullet *bullet = nullptr;
 	MyRaser *raser = nullptr;
-
+	
 	my_canvas.show([&]
 	{
-		// move tank(left & right side) - 방향키
-		if (my_canvas.isKeyPressed(GLFW_KEY_LEFT)) {
-			tank.center_.x -= 0.001; 
-		}
-		if (my_canvas.isKeyPressed(GLFW_KEY_RIGHT)) {
-			tank.center_.x += 0.001;
-		}
-
-		// move tank(up & down)
-		if (my_canvas.isKeyPressed(GLFW_KEY_UP)) {
-			tank.center_.y += 0.001;
-		}
-		if (my_canvas.isKeyPressed(GLFW_KEY_DOWN)) {
-			tank.center_.y -= 0.001;
-		}
-
-		// move warrior(left & right side) - WSAD
-		if (my_canvas.isKeyPressed(GLFW_KEY_A)) {
-			warrior.center_.x -= 0.001;
-		}
-		if (my_canvas.isKeyPressed(GLFW_KEY_D)) {
-			warrior.center_.x += 0.001;
-		}
-
-		// move warrior(up & down)
-		if (my_canvas.isKeyPressed(GLFW_KEY_W)) {
-			warrior.center_.y += 0.001;
-		}
-		if (my_canvas.isKeyPressed(GLFW_KEY_S)) {
-			warrior.center_.y -= 0.001;
-		}
+		warrior.keySettings();	// 키셋팅
+		gunner.keySettings();	// 키셋팅
 
 		// shoot a cannon ball
 		if (my_canvas.isKeyPressed(GLFW_KEY_SPACE))
@@ -256,7 +428,7 @@ int main(void)
 			{
 				if (bullet != nullptr) { delete bullet; }
 				bullet = new MyBullet;
-				bullet->center_ = tank.center_;
+				bullet->center_ = gunner.center_;
 				bullet->center_.x += 0.2;
 				bullet->center_.y += 0.1;
 				bullet->velocity_ = vec3(2.0, 0.0, 0.0);
@@ -277,9 +449,8 @@ int main(void)
 		if (raser != nullptr) raser->update(1 / 10.0f);
 
 		// rendering
-
-		tank.draw();
 		warrior.draw(time);
+		gunner.draw(time);
 
 		if (bullet != nullptr) bullet->draw();
 		if (raser != nullptr) raser->draw();
